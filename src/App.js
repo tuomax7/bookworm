7a1757e5a0dac34666777933fcaaf5d05d11ccca
Tuomas Nummela <tuomas.nummela.per@gmail.com>
2021-04-26 20:36:45 +0300

    Streak updating and totalPagesRead updating now works on a basic level.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 import './App.css';
 
@@ -8,37 +9,44 @@ import StreakView from './components/StreakView.js';
 import ReadingUpdater from './components/ReadingUpdater.js';
 
 
-//Variable declarations
-const books = [
-	{
-		'name': 'Bookname 1',
-		'pages': 100,
-		'pagesRead': 50
-	},
-	{
-		'name': 'Bookname 2',
-		'pages': 200,
-		'pagesRead': 200
-	}
-];
+const App = () => {
+	//Variable declarations
+	const books = [
+		{
+			'name': 'Bookname 1',
+			'pages': 100,
+			'pagesRead': 50
+		},
+		{
+			'name': 'Bookname 2',
+			'pages': 200,
+			'pagesRead': 200
+		}
+	];
 
-const streak = 0;
-const totalPages = 0;
+//State declarations
+const [ appState, setAppState ] = useState('start');
+const [ streak, setStreak ] = useState(0);
+const [ totalPages, setTotalPages ] = useState(0);
 
+
+//Function declarations
 
 const onReadingUpdateClick = () => {
 	//Make this render ReadingUpdater
+	setAppState('updatingReading')
 }
 
-
-const App = () => {
   return (
     <div>
     	<h1>Bookworm</h1>
   		
   		<StreakView streak={streak} totalPages={totalPages}/>
-  		<button onClick={onReadingUpdateClick}>Update read pages!</button>
-  		<ReadingUpdater />
+
+  		{appState === 'start' && (<button onClick={onReadingUpdateClick}>Update read pages!</button>)}
+
+  		{appState === 'updatingReading' && (<ReadingUpdater setAppState={setAppState} setStreak={setStreak} setTotalPages={setTotalPages} streak={streak} totalPages={totalPages}/>) }
+
   		<Booklist books={books}/>
 
     </div>
--- src/components/ReadingUpdater.js
+++ src/components/ReadingUpdater.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const ReadingUpdater = (props) => {
 
+
+	//Variable declarations
+
+
+	//State declarations
+	const [ pagesRead, setPagesRead ] = useState(0);
+
+	//Function declarations
+	const submitReading = () => {
+		props.setStreak(props.streak+1);
+		props.setTotalPages(props.totalPages+pagesRead);
+
+		props.setAppState('start');
+	}
+
 	return (
 		<div>
-			<h3>Update read pages</h3>
 			{/*Later change book selection to dropdown-menu*/}
+			<h3>Update read pages</h3>
+
 			<p>Book: <input type='text' placeholder='book name here...' /></p>
-			<p>Pages read: <input type='text' placeholder='read pages here...'/></p>
+			<p>Pages read: <input type='text' placeholder='read pages here...' 
+			onChange={event => setPagesRead(Number(event.target.value))} /></p>
+
+			<button onClick={submitReading}>Submit pages read!</button>
 		</div>
 	);
 }
