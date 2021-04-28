7010e50244ba61d5caa80e47d87d85e002d9d921
Tuomas Nummela <tuomas.nummela.per@gmail.com>
2021-04-28 18:54:19 +0300

    Time functionality work in progress.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import './App.css';
 
@@ -12,30 +12,44 @@ import ReadingUpdater from './components/ReadingUpdater.js';
 const App = () => {
 
 //State declarations
-const [ appState, setAppState ] = useState('start');
-const [ latestDayRead, setLatestDayRead ] = useState(new Date());
-const [ streak, setStreak ] = useState(0);
-const [ totalPages, setTotalPages ] = useState(0);
-const [ books, setBooks ] = useState([
-	{
-		'name': 'Bookname 1',
-		'pages': 100,
-		'pagesRead': 0
-	},
-	{
-		'name': 'Bookname 2',
-		'pages': 200,
-		'pagesRead': 0
-	}
-]);
+  const [ appState, setAppState ] = useState('start');
+  const [ streak, setStreak ] = useState(0);
+  const [ totalPages, setTotalPages ] = useState(0);
+  const [ books, setBooks ] = useState([
+	  {
+		  'name': 'Bookname 1',
+		  'pages': 100,
+		  'pagesRead': 0
+	  },
+	  {
+		  'name': 'Bookname 2',
+		  'pages': 200,
+		  'pagesRead': 0
+	  }
+  ]);
 
+  const [ latestDateRead, setLatestDateRead ] = useState(new Date("April 27, 2021"));
+  const [ readByDate, setReadByDate ] = useState(
+  	new Date(latestDateRead.getFullYear(), latestDateRead.getMonth(), latestDateRead.getDate()+1));
 
-//Function declarations
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
 
-const onReadingUpdateClick = () => {
-	//Make this render ReadingUpdater
-	setAppState('updatingReading')
-}
+  //Function declarations
+
+  const onReadingUpdateClick = () => {
+	  //Make this render ReadingUpdater
+	  setAppState('updatingReading')
+  }
+
+  //Handles streak resetting
+  useEffect(() => {
+  	if(today > readByDate){
+  		window.alert('You missed a day of reading, so your streak resets!');
+  		setStreak(0);
+  	}
+  }, [])
+
 
   return (
     <div>
@@ -45,7 +59,8 @@ const onReadingUpdateClick = () => {
 
   		{appState === 'start' && (<button onClick={onReadingUpdateClick}>Update read pages!</button>)}
 
-  		{appState === 'updatingReading' && (<ReadingUpdater setAppState={setAppState} setStreak={setStreak} setTotalPages={setTotalPages} streak={streak} totalPages={totalPages} books={books} setBooks={setBooks}/>) }
+  		{appState === 'updatingReading' && (<ReadingUpdater setAppState={setAppState} setStreak={setStreak} setTotalPages={setTotalPages} streak={streak} totalPages={totalPages} 
+  			books={books} setBooks={setBooks} todayDate={today} readByDate={readByDate}/>) }
 
   		<Booklist books={books}/>
 
