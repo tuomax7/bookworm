ea55c19c75e62e79bd1d95d879233a24dc780dba
Tuomas Nummela <tuomas.nummela.per@gmail.com>
2021-05-21 10:22:52 +0300

    Backend still work in progress.

--- back/index.js
+++ back/index.js
@@ -1,1 +1,41 @@
-console.log('hello world');
+const express = require('express')
+const app = express()
+
+let books = [
+  {
+    id: 1,
+    name: "Bookname 1",
+    pages: 100,
+    pagesRead: 0
+  },
+  {
+    id: 2,
+    name: "Bookname 2",
+    pages: 200,
+    pagesRead: 0
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/books', (req, res) => {
+  res.json(books)
+})
+
+app.get('/api/books/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const book = books.find(book => book.id === id)
+
+  if(book){
+  	response.json(book)
+  }else{
+  	response.status(404).end()
+  }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
